11ab6e294d8d773141c3d8a5d75ed5512865ffaa
Will Boyce <me@willboyce.com>
2019-09-19 09:58:44 +0100

    lint

--- publishconf.py
+++ publishconf.py
@@ -2,14 +2,16 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
 import os
 import sys
 
-sys.path.append(os.curdir)
 from pelicanconf import *
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+
+sys.path.append(os.curdir)
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = ''
